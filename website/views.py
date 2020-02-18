f6214b7d262d7028d4783ed53c7159a3530003ed
Nomi Khan <shegharkh@hotmail.com>
2020-02-18 15:08:10 -0800

    Initial Commit

--- website/views.py
+++ website/views.py
@@ -32,3 +32,49 @@ def pricing(request):
 
 def service(request):
     return render(request, 'service.html', {})
+
+def appointment(request):
+    if request.method == "POST":
+        your_name = request.POST['your-name']
+        your_phone = request.POST['your-phone']
+        your_email = request.POST['your-email']
+        your_address = request.POST['your-address']
+        your_schedule = request.POST['your-schedule']
+        your_date = request.POST['your-date']
+        your_message = request.POST['your-message']
+
+        #send email
+        appointment = "Name: " + your_name + "Phone: " + your_phone + "E-mail: " + your_email + "Address : " + your_address + "Schedule: " + your_schedule + "Date: " + your_date + "Message: " + your_message
+        send_mail(
+            'Appointment Request',
+            appointment,
+            your_email,
+            ['shegharkh@hotmail.com'],
+        )
+
+        return render(request, 'appointment.html', {
+            'your_name': your_name,
+            'your_phone': your_phone,
+            'your_email': your_email,
+            'your_address': your_address,
+            'your_schedule': your_schedule,
+            'your_date': your_date,
+            'your_message': your_message
+                                                    })
+
+    else:
+          return render(request, 'home.html', {})
+
+    #     message_name = request.POST['message-name']
+        #     message_email = request.POST['message-email']
+        #      message = request.POST['message']
+
+        #send mail
+
+        #     send_mail(
+        #        message_name , # subject
+        #        message, # message
+        #        message_email , # from email
+        #       ['shegharkh@hotmail.com'], # to email
+        #    )
+
