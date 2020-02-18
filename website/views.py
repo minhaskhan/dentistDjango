68e792b109ca2e557d4d15ec38dae20571c276a2
Nomi Khan <shegharkh@hotmail.com>
2020-02-18 01:39:31 -0800

    make it better

---
+++ website/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.core.mail import  send_mail
+
+def home(request):
+    return render(request, 'home.html', {})
+
+def contact(request):
+    if request.method == "POST":
+         message_name = request.POST['message-name']
+         message_email = request.POST['message-email']
+         message = request.POST['message']
+
+         #send mail
+         send_mail(
+            message_name , # subject
+            message, # message
+            message_email , # from email
+            ['shegharkh@hotmail.com'], # to email
+         )
+
+         return render(request, 'contact.html', {'message_name': message_name})
+
+    else:
+        return render(request, 'contact.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def pricing(request):
+    return render(request, 'pricing.html', {})
+
+def service(request):
+    return render(request, 'service.html', {})
